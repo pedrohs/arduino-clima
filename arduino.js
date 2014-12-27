f35b72378cc84ef354522a55a562ae1df5c345c6
pedrohs <9.pedro.deoliveira@gmail.com>
2014-12-27 14:27:18 -0200

    arrumado bugs, e adicionado mais funções

--- arduino.js
+++ arduino.js
@@ -1,7 +1,7 @@
 var five =  require('johnny-five'),
 clima = require("./clima.js"),
 arduino = five.Board('/dev/ttyACM0'),
-dadosClima,
+dadosClima = new Array(),
 lcd;
 
 arduino.on('ready', function(){
@@ -13,9 +13,13 @@ arduino.on('ready', function(){
 		cols: 20
 	});
 
-	lcd.on('ready', function(){
+	lcd.on('ready', function(){		
 		console.log("LCD Pronto");		
+		lcd.createChar("lupa", [31,17,10,4,10,17,31,0]);
+		lcd.createChar("graus", [0,14,10,14,0,0,0,0]);
 		lcd.clear().cursor(0,0).print("Carregando");
+		lcd.cursor(1,1).print("Informacoes :lupa:");
+		clima.ready();
 	});
 });
 
@@ -23,9 +27,49 @@ exports.pegaDados = function(status, dados){
 	if(status){
 		dadosClima = dados;		
 		mostraLcd(true);	
+	}else{
+		mostraLcd(false);
 	}
 }
 
 function mostraLcd(status){
-	lcd.print(dadosClima['temp']);
+	if(status){		
+		frameLcd();
+		arduino.loop(5000, frameLcd);
+	}else{
+		lcd.clear().print("Erro Encontrado");
+	}
+}
+
+var frame = 1;
+function frameLcd(){	
+	switch(frame){
+		case 1:
+			lcd.clear().print("Dados Climaticos");
+			lcd.cursor(1,0).print("Hoje " + dadosClima['dias'][0]['date']);
+			break;
+		case 2:
+			lcd.clear().print("Temperatura: " + dadosClima['temp']);
+			lcd.cursor(1,0).print("Umidade: " + dadosClima['umid']);
+			break;
+		case 3:
+			lcd.clear().print("Vento: " + dadosClima['vento']);
+			lcd.cursor(1,0).print("Visib: " + dadosClima['visib']);
+			break;
+		case 4:
+			lcd.clear().print("Dados Climaticos");
+			lcd.cursor(1,0).print(dadosClima['dias'][1]['date'] + " -->");
+			break;
+		case 5: 
+			lcd.clear().print("Temp: Max: " + dadosClima['dias'][1]['high'] + ":graus:");
+			lcd.cursor(1,6).print("Min: " + dadosClima['dias'][1]['low'] + ":graus:");
+			break;
+		case 6:
+			var code = dadosClima['dias'][1]['code'];
+			lcd.clear().print(dadosClima['city'] + "/" + dadosClima['estado']);
+			lcd.cursor(1,0).print(dadosClima['code'][code]);
+			break;
+	}
+	if(frame == 6){frame = 0};
+	frame++;
 }
--- clima.js
+++ clima.js
@@ -3,30 +3,36 @@ tempGraus,
 umidade,
 ventoVelocidade,
 dados = new Array(),
-arduino = require("./arduino.js");
+arduino = require("./arduino.js"),
+codeClima = new Array();
 
 function pegaDados(callback){
-	var URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22marilia%2C%20sp%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+	var URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D455882%20and%20u%3D%22c%22&format=json&diagnostics=true&callback=';
 	request(URL, function(error, response, body){
 		if (!error && response.statusCode == 200) {
 			var body = JSON.parse(body);
-			tempGraus = body['query']['results']['channel']['item']['condition']['temp'];
-			tempGraus = tempGraus - 32;
-			tempGraus = tempGraus / 1.8;
-			tempGraus = tempGraus.toFixed(0);
+			dados['temp'] = body['query']['results']['channel']['item']['condition']['temp'];
+			dados['temp'] = dados['temp'] + ":graus:";
 
-			umidade = body['query']['results']['channel']['atmosphere']['humidity'];
-			ventoVelocidade = body['query']['results']['channel']['wind']['speed'];
-			ventoVelocidade = ventoVelocidade * 1.609344;
-			ventoVelocidade = ventoVelocidade.toFixed(1);
-			ventoVelocidade = ventoVelocidade + "Km/h";
+			dados['umid'] = body['query']['results']['channel']['atmosphere']['humidity'];
+			dados['umid'] = dados['umid'] + "%"
 
-			dados['temp'] = tempGraus;
-			dados['umid'] = umidade;
-			dados['vento'] = ventoVelocidade;
+			dados['vento'] = body['query']['results']['channel']['wind']['speed'];
+			dados['vento'] = dados['vento'] * 1.609344;
+			dados['vento'] = dados['vento'].toFixed(1);
+			dados['vento'] = dados['vento'] + "Km/h";
+
+			dados['visib'] = body['query']['results']['channel']['atmosphere']['visibility'];
+			dados['visib'] = dados['visib'] + "mi";
+
+			var dias = body['query']['results']['channel']['item']['forecast'];
+			dados['dias'] = dias;
+			dados['code'] = codeClima;
+
+			dados['city'] = body['query']['results']['channel']['location']['city'];
+			dados['estado'] = body['query']['results']['channel']['location']['region'];
 
 			callback(true);
-
 		}else{
 			console.log(error);
 			callback(false);
@@ -34,13 +40,66 @@ function pegaDados(callback){
 	});
 }
 
-pegaDados(app);
-
 function app(estado){
 	if(estado){
 		console.log("dados pegos")
 		arduino.pegaDados(true, dados);
 	}else{
 		console.log("erro nos dados");
+		arduino.pegaDados(false);
 	}
 }
+
+exports.ready = function(){
+	pegaDados(app);
+}
+
+codeClima[0] = 'tufão';
+codeClima[1] = 'tempestade tropical';
+codeClima[2] = 'furacão';
+codeClima[3] = 'temporais severos';
+codeClima[4] = 'temporais';
+codeClima[5] = 'chuva mista e neve';
+codeClima[6] = 'chuva mista e granizo';
+codeClima[7] = 'neve mista e granizo';
+codeClima[8] =  'garoa que se congela';
+codeClima[9] =  'garoa';
+codeClima[10] = 'chuva que se congela';
+codeClima[11] = 'chuvas';
+codeClima[12] = 'chuvas';
+codeClima[13] = 'lufadas de neve';
+codeClima[14] = 'chuvas de neve fraca';
+codeClima[15] = 'nevasca';
+codeClima[16] = 'neve';
+codeClima[17] = 'granizo';
+codeClima[18] = 'granizo';
+codeClima[19] = 'pó';
+codeClima[20] = 'nebulosos';
+codeClima[21] = 'neblina';
+codeClima[22] = 'fumegantes';
+codeClima[23] = 'estrondosos';
+codeClima[24] = 'ventosos';
+codeClima[25] = 'frio';
+codeClima[26] = 'nublados';
+codeClima[27] = 'pela maior parte nublados (noite)';
+codeClima[28] = 'pela maior parte nublados (dia)';
+codeClima[29] = 'em parte nublado (noite)';
+codeClima[30] = 'em parte nublado (dia)';
+codeClima[31] = 'claros (noite)';
+codeClima[32] = 'cheios de sol';
+codeClima[33] = 'feira (noite)';
+codeClima[34] = 'feira (dia)';
+codeClima[35] = 'chuva mista e granizo';
+codeClima[36] = 'quentes';
+codeClima[37] = 'temporais isolados';
+codeClima[38] = 'temporais espalhados';
+codeClima[39] = 'temporais espalhados';
+codeClima[40] = 'chuvas espalhadas';
+codeClima[41] = 'neve pesada';
+codeClima[42] = 'chuvas de neve espalhadas';
+codeClima[43] = 'neve pesada';
+codeClima[44] = 'em parte nublado';
+codeClima[45] = 'thundershowers';
+codeClima[46] = 'chuvas de neve';
+codeClima[47] = 'temporais isolados';
+codeClima[3200] = 'não disponível';
